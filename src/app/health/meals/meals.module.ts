f015825ac6749e94013d938337d841c1b95220a3
Oleksandr Hrybovoda <oleksandr.grybovoda@embrox.com>
2023-04-03 10:31:59 +0300

    added meal

--- src/app/health/meals/meals.module.ts
+++ src/app/health/meals/meals.module.ts
@@ -3,17 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MealsComponent } from './components/meals/meals.component';
-import { SharedModule } from '../shared/shared.module';
+import { MealComponent } from './components/meal/meal.component';
 
-export const ROUTES: Routes = [{ path: '', component: MealsComponent }];
+export const ROUTES: Routes = [
+  { path: '', component: MealsComponent },
+  { path: 'new', component: MealComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    SharedModule,
-    RouterModule.forChild(ROUTES),
-  ],
-  declarations: [MealsComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(ROUTES)],
+  declarations: [MealsComponent, MealComponent],
 })
 export class MealsModule {}
